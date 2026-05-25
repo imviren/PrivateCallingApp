082ef4521751c001044fa393d20294c787a07337
imviren <imviren@users.noreply.github.com>
2026-05-25 18:32:46 +0530

    Rename root project and launcher name to PrivateCallingApp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "Communication"
+rootProject.name = "PrivateCallingApp"
 include(":app")
 include(":core-network")
 include(":core-media")
